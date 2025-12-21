1a9acd4c8cbc5c75793bb049ebc7a99fcea29c62
Abz <220621470+abz-k@users.noreply.github.com>
2025-12-21 11:32:26 +0300

    Solve 08_removefromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  for (let arg in args) {
+    while (array.includes(args[arg])) {
+      let index = array.indexOf(args[arg]);
+      array.splice(index, 1);
+    }
+  }
+  return array;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
