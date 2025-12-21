b6d862672b2f262a2ddd634b6b1834e1ff858f58
Abz <220621470+abz-k@users.noreply.github.com>
2025-12-21 16:14:04 +0300

    Solve 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degInF) {
+  let result = (degInF - 32) * (5 / 9);
+  return parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degInC) {
+  let result = degInC * (9 / 5) + 32;
+  return parseFloat(result.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
