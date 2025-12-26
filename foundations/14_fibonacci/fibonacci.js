a56115572a1c1f2f0ca6021818472525cebc2e57
Abz <220621470+abz-k@users.noreply.github.com>
2025-12-26 20:44:40 +0300

    Complete fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (numOfFibMember) {
+  numOfFibMember = parseInt(numOfFibMember);
+  if (numOfFibMember < 0) {
+    return "OOPS";
+  }
 
+  const fib = [0, 1];
+  for (let i = 2; i < numOfFibMember + 1; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib[numOfFibMember];
 };
 
 // Do not edit below this line
