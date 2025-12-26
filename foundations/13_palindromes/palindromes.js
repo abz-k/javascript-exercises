d3e0d4fe4aa976202e2ec8400e7e8c3fd7395a4e
Abz <220621470+abz-k@users.noreply.github.com>
2025-12-26 19:52:32 +0300

    Complete 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const cleanedString = string.replace(/[\s,.!]/g, "").toLowerCase();
+  let cleanArray = Array.from(cleanedString);
+  console.log(cleanArray);
+  let part1 = cleanArray.slice(0, cleanArray.length / 2);
+  let part2 = cleanArray.slice(cleanArray.length / 2).reverse();
+  console.log(part1, part2);
 
+  return part1.every((element, index) => element === part2[index]);
 };
 
 // Do not edit below this line
